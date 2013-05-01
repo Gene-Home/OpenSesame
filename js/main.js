fc81e789b998fbc9e95f4f3ffbbc3d5663835f9d
ghdulrich <ghdulrich@gmail.com>
2013-04-30 21:22:44 -0400

    added cookie mgmt and navigation changes

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ var AppRouter = Backbone.Router.extend({
     userToken :'',
     progressTemplate: _.template($('#tpl-progress').html()),
     routes:{
-	"":"showLanding",
-	"showHome":"showHome",
+	"":"showHomePublic",
+	"showHome":"showHomePrivate",
         "jobRuns":"listJobRuns",
 	"seriesResults/:wFileID/:resultName":"listSeriesResults",
 	"newGeneSig":"newGeneSig",
@@ -17,23 +17,60 @@ var AppRouter = Backbone.Router.extend({
 	"sendReset":"sendResetEmail",
 	"resetPassword/:username/:token":"resetPassword"
     },
-    showLanding:function(){
+    initialize: function () {
+	// if user logged on - show side bar
+	// and logged in top nav
+	var coo = $.cookie("userToken");
+	var ucoo = $.cookie("user");
+	if (coo){
+	    this.userToken = coo;
+	    var tuser = JSON.parse(ucoo);
+	    // since the object stored in the cookie is not 
+	    // a full blown backbone object, we need to create the user
+	    this.user = new User();
+	    this.user.set('username',tuser.username);
+	    this.user.set('password',tuser.password);
+	    this.setupPagePrivate();
+	    
+	    
+	}else{
+	    this.showHomePublic();
+	}
+	return false;
+    },
+    showHomePublic:function(){
+	this.setupPagePublic();
 	$.get("landing.html", function(content){
             var html = content;
-	    var ma =  $('#main-area'); 
-	    ma.attr('class','span12');
+	    var ma =  $('#main-area');
             ma.html(html);
 	});
 	return false;
     },
-    showHome:function(){
+    showHomePrivate:function(){
+	this.setupPagePrivate();
 	$.get("overview.html", function(content){
             var html = content;
-	    var ma =  $('#main-area'); 
+	    var ma =  $('#main-area');
             $('#main-area').html(html);
 	});
 	return false;
     },
+    setupPagePublic:function(){
+	var ma =  $('#main-area');
+	// set to 12, this is what a logged out
+	// user sees - fill the whole screen
+	ma.attr('class','span12');
+    },
+   setupPagePrivate:function(){
+       var ma =  $('#main-area');
+       // set to 9, this is what a logged in
+       // user sees - to the right of the sidebar
+       ma.attr('class','span9');
+       var navbar_template =  _.template($('#tpl-navbar-yes-login').html());
+       $('#navbar').html(navbar_template(this.user.toJSON()));
+       $('#sidebar').show();
+   }, 
     listSeriesResults:function (wFileID,resultName) {
         this.seriesResultList = new SeriesResultCollection();
 	// should be able to just make this a workfile contents model -- and set the id
@@ -72,27 +109,43 @@ var AppRouter = Backbone.Router.extend({
     },
     saveGeneSig:function(){
 	app.geneSigView.saveGeneSig(this.listJobRuns);
+	// make sure the url is set to #jobRuns in case the
+	// user refreshes the browser
+	app.navigate("jobRuns", {trigger: false, replace: true});
 	return false;
     },
     newUser:function(){
+	// close any other modals
+	$(".modal").remove();
+	// since this is a modal - we are going to need to reset
+	// the url in the browser
+	app.navigate("", {trigger: false, replace: true});
 	app.user = new User();
 	app.newUserView = new UserView({model:app.user});
 	app.newUserView.show();
 	return false;
     },
     login:function(){
+	// close any other modals
+	$(".modal").remove();
 	app.user = new User();
 	app.loginView = new LoginView({model:app.user});
 	app.loginView.show();
-	
 	return false;
     }, 
     logout:function(){
+	$.removeCookie("userToken");
 	app.user = new User();
 	window.location = 'index.html';
 	return false;
     },
     sendResetEmail:function(){
+	// close any other modals
+	$(".modal").remove();
+	// since this is a modal - we are going to need to reset
+	// the url in the browser
+	app.navigate("", {trigger: false, replace: true});
+	// close the other modal
 	app.sendResetView = new SendResetView({model:app.user});
 	app.sendResetView.show();
 	return false;
@@ -113,12 +166,6 @@ var AppRouter = Backbone.Router.extend({
 });
 
 var app = new AppRouter();
-// need to change the url so that we can 
-// click twice on the same link
-app.bind('all', function(route) {
-   app.navigate("", {trigger: false, replace: true});
-    
-});
 
 $("#sendReset").click( function(){
     // window.
@@ -130,4 +177,5 @@ $(".nav li").click(function(e) {
     $(this).addClass("active");
     
 });
+
 Backbone.history.start();
